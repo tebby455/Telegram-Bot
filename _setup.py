ebb6c67603a9f6588d531455ae810a93378724bd
7heKnight <60308445+7heKnight@users.noreply.github.com>
2021-07-15 20:21:33 +0700

    Add files via upload
    Add setting file

---
+++ _setup.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python3
+import time
+import os
+app = ['python-telegram-bot', 'bs4', 'requests', 'psutil']
+print('[+] Setting up...')
+time.sleep(0.5)
+for name in app:
+    os.system(f'pip install {name}')
