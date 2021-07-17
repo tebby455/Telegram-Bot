f1a3aba1725bf5f3cfbf183cff5212125e17d828
7heKnight <60308445+7heKnight@users.noreply.github.com>
2021-07-17 10:55:31 +0700

    Update _setup.py
    Add lib PyCryptodome for _setup.py

--- _setup.py
+++ _setup.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
 import time
 import os
-app = ['python-telegram-bot', 'bs4', 'requests', 'psutil']
+app = ['python-telegram-bot', 'bs4', 'requests', 'psutil', 'PyCryptodome']
 print('[+] Setting up...')
 time.sleep(0.5)
 for name in app:
